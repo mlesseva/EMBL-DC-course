17015e6b5be321f2a95d3904fb3f6ea276d15c8b
mlesseva <mlesseva@gmail.com>
2024-01-23 16:52:31 +0200

    R code

---
+++ First code.R
@@ -0,0 +1,23 @@
+3+4
+weight_kg <- 55
+Weight_kg <- 34
+(my_weight <- 50)
+
+
+2.2 * weight_kg
+
+weight_kg <- 57
+2.2 * weight_kg
+
+weight_lb <- 2.2*weight.kg
+weight_kg <- 100
+
+
+
+
+
+
+
+
+
+
