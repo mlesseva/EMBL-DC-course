d4238ed3fc8d9f183a2f393b58bedf2a840c2ea6
mlesseva <mlesseva@gmail.com>
2024-01-24 12:59:01 +0200

    Arranging

--- scripts/Using Dplyr and ggplot.R
+++ scripts/Using Dplyr and ggplot.R
@@ -47,6 +47,89 @@ surveys %>%
   mutate(weight_kg = weight/1000, weight_lb = weight_kg * 2.2) %>%
   head()
 
+surveys %>% 
+  filter(!is.na(weight)) %>% 
+  mutate(weight_kg = weight/1000, weight_lb = weight_kg * 2.2) %>%
+  select(-weight)
+
+#to compare average weights of males to females (split-apply-combine)
+surveys %>% 
+  group_by(sex) %>% 
+  summarize(mean_weight = mean(weight, na.rm = TRUE))
+#removing NA rows
+surveys %>% 
+  filter(!is.na(sex)) %>% 
+  group_by(sex) %>% 
+  summarize(mean_weight = mean(weight, na.rm = TRUE))
+
+surveys %>% 
+  filter(!is.na(weight)) %>% 
+  group_by(sex, species_id) %>% 
+  summarize(mean_weight = mean(weight, na.rm = T)) %>% 
+  tail()
+
+surveys %>% 
+  filter(!is.na(weight)) %>% 
+  group_by(sex, species_id) %>% 
+  summarize(mean_weight = mean(weight, na.rm = T)) %>% 
+  print(n=15)
+
+surveys %>% 
+  filter(!is.na(weight), !is.na(sex)) %>% 
+  group_by(sex, species_id) %>% 
+  summarize(mean_weight = mean(weight, na.rm = T)) %>% 
+  print(n=15)
+
+#arrange by min weight
+surveys %>% 
+  filter(!is.na(weight), !is.na(sex)) %>% 
+  group_by(sex, species_id) %>% 
+  summarize(mean_weight = mean(weight, na.rm = T), min_weight = min(weight)) %>% 
+  arrange(min_weight)
+
+#arrange by descending order
+surveys %>% 
+  filter(!is.na(weight), !is.na(sex)) %>% 
+  group_by(sex, species_id) %>% 
+  summarize(mean_weight = mean(weight, na.rm = T), min_weight = min(weight)) %>% 
+  arrange(desc(min_weight))
+
+#arrange by ascending order
+surveys %>% 
+  filter(!is.na(weight), !is.na(sex)) %>% 
+  group_by(sex, species_id) %>% 
+  summarize(mean_weight = mean(weight, na.rm = T), min_weight = min(weight)) %>% 
+  arrange((min_weight))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
