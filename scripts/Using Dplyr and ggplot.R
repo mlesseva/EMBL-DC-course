c0f3940beeac76d71a18c57a492471f08b83547c
mlesseva <mlesseva@gmail.com>
2024-01-24 15:39:21 +0200

    created new table

--- scripts/Using Dplyr and ggplot.R
+++ scripts/Using Dplyr and ggplot.R
@@ -149,7 +149,39 @@ surveys %>%
   arrange(year) %>% 
   unique()
   
+#to better compare genus and weight for different genra (long to wide table format)
+surveys_gw <- surveys %>% 
+  filter(!is.na(weight)) %>% 
+  group_by(plot_id, genus) %>% 
+  summarize(mean_weight = mean(weight))
 
+str(surveys_gw)
+
+surveys_wide <- surveys_gw %>%
+  pivot_wider(names_from = genus, values_from = mean_weight, values_fill = 0)
+  
+#going from wide to long format (separate columns)
+#surveys_wide <- surveys_gw
+surveys_wide %>% 
+    pivot_longer(names_to = "genus", values_to = "mean_weight", cols = -plot_id)
+
+#challenge
+surveys_long <- surveys %>% 
+  pivot_longer(names_to = "measurement", values_to = "values", cols = c(hindfoot_length, weight)) %>% 
+  View()
+
+surveys_long %>% 
+  group_by(year, measurement, plot_type) %>%
+  summarize(mean_value = mean(value, na.rm = TRUE)) %>% 
+  pivot_wider(names_from = measurement, values_from = mean_value)
+
+
+
+
+surveys_complete <- surveys %>% 
+  filter(!is.na(weight), !is.na(hindfoot_length), !is.na(sex))
+
+write_csv(surveys_complete, file = "surveys_complete.csv")
 
 
 
